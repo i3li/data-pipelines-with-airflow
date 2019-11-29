e2071a2e09af0193f3f4469c951bd459dfc5eddb
Ali Za <i3li.xz@gmail.com>
2019-11-29 03:44:35 +0300

    docs: Add missing documentation

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -4,6 +4,7 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class LoadDimensionOperator(BaseOperator):
+    """This operator enables loading dimension tables."""
 
     ui_color = '#F98866'
 
@@ -15,7 +16,20 @@ class LoadDimensionOperator(BaseOperator):
                  select_sql_stmt='',
                  truncate_insert=True,
                  *args, **kwargs):
-
+        """
+        :param redshift_conn_id: The redshift connection id. The name or identifier for
+        establishing a connection to Redshift.
+        :type redshift_conn_id: str
+        :param aws_credentials_id: The AWS connection id. The name or identifier fo
+        establishing a connection to AWS
+        :type aws_credentials_id: str
+        :param table: The name of the table where the data should be loadede.
+        :type table: str
+        :param select_sql_stmt: The SQL statment that is used to retrieve the fact table data.
+        :type select_sql_stmt: str
+        :param truncate_insert: Wheter to trucate the target table before insertion or not.
+        :type truncate_insert: bool
+        """
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
         self.aws_credentials_id = aws_credentials_id
@@ -27,17 +41,16 @@ class LoadDimensionOperator(BaseOperator):
         # AWS Hook
         aws_hook = AwsHook(self.aws_credentials_id)
         credentials = aws_hook.get_credentials()
-        
+
         # Postgres Hook
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
-        
+
         insert_sql_stmt = 'INSERT INTO {} ({})'.format(self.table, self.select_sql_stmt)
-        
+
         if self.truncate_insert:
             truncate_sql_stmt='DELETE FROM {}'.format(self.table)
             # Truncate table
             redshift.run(truncate_sql_stmt)
-        
+
         # Load table
         redshift.run(insert_sql_stmt)
-
--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -4,6 +4,7 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class LoadFactOperator(BaseOperator):
+    """This operator enables loading fact tables."""
 
     ui_color = '#F98866'
 
@@ -14,7 +15,18 @@ class LoadFactOperator(BaseOperator):
                  table='',
                  select_sql_stmt='',
                  *args, **kwargs):
-
+        """
+        :param redshift_conn_id: The redshift connection id. The name or identifier for
+        establishing a connection to Redshift.
+        :type redshift_conn_id: str
+        :param aws_credentials_id: The AWS connection id. The name or identifier fo
+        establishing a connection to AWS
+        :type aws_credentials_id: str
+        :param table: The name of the table where the data should be loadede.
+        :type table: str
+        :param select_sql_stmt: The SQL statment that is used to retrieve the fact table data.
+        :type select_sql_stmt: str
+        """
         super(LoadFactOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
         self.aws_credentials_id = aws_credentials_id
@@ -25,12 +37,11 @@ class LoadFactOperator(BaseOperator):
         # AWS Hook
         aws_hook = AwsHook(self.aws_credentials_id)
         credentials = aws_hook.get_credentials()
-        
+
         # Postgres Hook
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
-        
+
         insert_sql_stmt = 'INSERT INTO {} ({})'.format(self.table, self.select_sql_stmt)
-        
+
         # Load table
         redshift.run(insert_sql_stmt)
-
